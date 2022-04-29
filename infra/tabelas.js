9811a8a81a0f88c8a49132413441fe456eef7d08
Lucas Kenzo <kenzoawane@gmail.com>
2022-04-29 12:04:44 -0300

    Update tabelas.js

--- infra/tabelas.js
+++ infra/tabelas.js
@@ -9,7 +9,19 @@ class Tabelas {
     const sql =
       "CREATE TABLE despesa (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, valor DOUBLE NOT NULL, descricao VARCHAR(150) NOT NULL)";
     this.connection.query(sql);
-    }
+  }
+  /*
+  criaCarteira() {
+    const sql = 'CREATE TABLE carteira' +
+      '(id_pk int NOT NULL,' +
+      'descricao varchar(100),' +
+      'saldo double NOT NULL,' +
+      'limite double NOT NULL,' +
+      'nome varchar(100) NOT NULL),' +
+      'FOREIGN KEY (id_pk) REFERENCES login (id_pk)'
+    this.connection.query(sql);
+  }
+  */
 }
 
 module.exports = new Tabelas;
