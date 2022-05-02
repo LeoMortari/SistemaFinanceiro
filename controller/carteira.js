f44844b1bde4cf773966192db3ea8b0934e0e3c5
Lucas Kenzo <kenzoawane@gmail.com>
2022-05-01 23:18:34 -0300

    adicionando login
    add login com agls testes de validacoes

--- controller/carteira.js
+++
@@ -1,8 +0,0 @@
-module.exports=(app)=>{
-    app.get('/carteira',(req,res)=>
-    res.send("voce esta em carteira"))
-    app.post('/carteira',(req,res)=>{
-        console.log(req.body);
-    res.send("voce esta via post")
-    })
-    }
---
+++ controller/login/index.js
@@ -0,0 +1,51 @@
+const mysql = require("../../infra/connection");
+
+module.exports = (app) => {
+  app.post("/login", (req, res) => {
+    let { email, id_pk, senha } = req.body;
+
+    const SQL = `INSERT INTO login (email,id_pk,senha) VALUES ('${email}', ${id_pk}, '${senha}')`;
+
+    mysql.query(SQL, (err, result) => {
+      if (err) return res.send("Não foi possivel adicionar um novo lançamento");
+
+      if (result.message) {
+        const { message } = result;
+
+        return res.send(message);
+      }
+    });
+
+    //testando validação
+    const Selecionar = `SELECT * FROM login`;
+    mysql.query(Selecionar, (err, result) => {
+      if (err) return res.send("Não foi possivel adicionar um novo lançamento");
+
+      //validando email
+      if (email.match(/@/)) {
+        if (email.match(/\./)) {
+          console.log("email valido")
+        } else {
+          console.log("email inválido")
+        }
+      }
+
+      //validando senha
+      if(senha.length>7){
+        console.log("senha válida")
+        
+      }else{
+        console.log("senha inválida")
+      }
+
+      if (result.message) {
+        const { message } = result;
+
+        return res.send(message);
+      }
+    }
+    );
+    console.log(req.body);
+    res.send("login adicionado");
+  });
+};
